8672fd8d618c3aabe9e403352e62f34683d1eaf7
hekl0 <sathubian1999@gmail.com>
2019-04-11 17:52:53 -0400

    push

--- wwwroot/js/game.js
+++ wwwroot/js/game.js
@@ -10,3 +10,38 @@ let config = {
 };
 
 let game = new Phaser.Game(config);
+
+const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/gameHub")
+    .build();
+
+connection.start().then(function () {
+    console.log("connected");
+});
+
+async function start() {
+    try {
+        await connection.start();
+        console.log("connected");
+    } catch (err) {
+        console.log(err);
+        setTimeout(() => start(), 5000);
+    }
+};
+
+connection.onclose(async () => {
+    await start();
+});
+
+connection.on("", () => {
+
+});
+
+document.addEventListener('keydown', function(event) {
+    if(event.keyCode == 37) {
+        alert('Left was pressed');
+    }
+    else if(event.keyCode == 39) {
+        alert('Right was pressed');
+    }
+});
