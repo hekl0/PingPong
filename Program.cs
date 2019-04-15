aaea35f06e99f07c5a9a6239b1a5602224e326a1
CallmeB <btran8@u.rochester.edu>
2019-04-15 19:00:29 -0400

    save changes

--- Program.cs
+++ Program.cs
@@ -18,7 +18,6 @@ namespace FinalProject
     public class Program
     {
 
-<<<<<<< HEAD
         public static void Main (string[] args) {
             //hubContext = GlobalHost.ConnectionManager.GetHubContext< GameHub>();
             var host = CreateWebHostBuilder (args).Build ();
@@ -34,30 +33,6 @@ namespace FinalProject
                         }
                     } catch (Exception ex) {
 
-=======
-        public static void Main(string[] args)
-        {
-            List<Pong> games = new List<Pong>();
-            //hubContext = GlobalHost.ConnectionManager.GetHubContext< GameHub>();
-            var host = CreateWebHostBuilder(args).Build();
-            Timer timer = new Timer(1000);
-            timer.Elapsed += (Object source, System.Timers.ElapsedEventArgs e) =>
-            {
-                    try
-                    {
-                        GameHub.pong.calculate();
-                        var hubContext = host.Services.CreateScope().ServiceProvider.GetRequiredService<IHubContext<GameHub>>();
-                        hubContext.Clients.All.SendAsync("Test", "gud shit");
-                        hubContext.Clients.All
-                            .SendAsync("ReceiveData",
-                                 GameHub.pong.x, GameHub.pong.y, 
-                                 GameHub.pong.paddle[1].x, Constants.upperPaddle, 
-                                 GameHub.pong.paddle[2].x, Constants.lowerPaddle);
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine(ex);
->>>>>>> c80f11a3a6749374a84330e3841c2f4c66185348
                     }
                 
 
@@ -66,11 +41,7 @@ namespace FinalProject
             timer.AutoReset = true;
             timer.Enabled = true;
 
-<<<<<<< HEAD
             host.Run ();
-=======
-            host.Run();
->>>>>>> c80f11a3a6749374a84330e3841c2f4c66185348
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
