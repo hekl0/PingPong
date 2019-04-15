76188a4eaa74265c8e6cac9dd9acd964cac7f2a0
CallmeB <btran8@u.rochester.edu>
2019-04-15 01:17:40 -0400

    timer

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FinalProject.Hubs;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -9,57 +10,57 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using FinalProject.Hubs;
+using Microsoft.AspNetCore.SignalR;
 
-namespace FinalProject
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace FinalProject {
+    public class Startup {
+
+        public class CallingSideClass {
+            private readonly IHubContext<GameHub> _hubContext;
+
+            public CallingSideClass (IHubContext<GameHub> hubContext) {
+                _hubContext = hubContext;
+            }
+
+        }
+
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
+        public void ConfigureServices (IServiceCollection services) {
+            services.Configure<CookiePolicyOptions> (options => {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-            services.AddSignalR();
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddSignalR ();
+            services.AddScoped<CallingSideClass>();
+            services.AddMvc ().SetCompatibilityVersion (CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
+        public void Configure (IApplicationBuilder app, IHostingEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
+            } else {
+                app.UseExceptionHandler ("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.UseHsts ();
             }
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseCookiePolicy();
-            app.UseSignalR(routes =>
-            {
-                routes.MapHub<GameHub>("/gameHub");
+            app.UseHttpsRedirection ();
+            app.UseStaticFiles ();
+            app.UseCookiePolicy ();
+            app.UseSignalR (routes => {
+                routes.MapHub<GameHub> ("/gameHub");
             });
 
-            app.UseMvc();
+            app.UseMvc ();
         }
     }
 }
