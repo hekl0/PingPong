b2f3c1f2ac64f23b1e2ec4b3dbebd7d0488c938a
hekl0 <sathubian1999@gmail.com>
2019-04-15 22:25:48 -0400

    finish test page

--- wwwroot/js/test.js
+++ wwwroot/js/test.js
@@ -5,6 +5,8 @@ $("#theme-kitchen").click(function() {
     theme_choice = 1;
     $("#censor-kitchen").attr("hidden", true);
     $("#censor-space").removeAttr("hidden");
+    $("#theme-kitchen").addClass("choosed");
+    $("#theme-space").removeClass("choosed");
 });
 
 $("#theme-space").click(function() {
@@ -12,4 +14,7 @@ $("#theme-space").click(function() {
     theme_choice = 2;
     $("#censor-kitchen").removeAttr("hidden");
     $("#censor-space").attr("hidden", true);
+    console.log("lol");
+    $("#theme-kitchen").removeClass("choosed");
+    $("#theme-space").addClass("choosed");
 });
