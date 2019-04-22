3a203887e184c53b156249beaf935f0984240675
UnknownPerson <UnknownPersonEmail@gmail.com>
2019-04-22 19:50:24 -0400

    some Changes

---
+++ Migrations/UserDBMigrations/20190422234447_InitialCreate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FinalProject.Migrations.UserDBMigrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<string>(nullable: false),
+                    password = table.Column<string>(nullable: true),
+                    room = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
